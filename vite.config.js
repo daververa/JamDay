cf784298eeee6b396d3242a297ac292d508069b6
daververa99 <daververa99@gmail.com>
2023-11-06 22:01:08 -0500

    Second commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+
     plugins: [
         laravel({
             input: [
